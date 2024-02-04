3176026dc2882caa0ede88512c85b78f57fdda10
AnferneeLai <50390611+AnferneeLai@users.noreply.github.com>
2024-02-04 02:29:01 -0800

    Added all 4 attack patterns with suits

--- Godot Project/scripts/PlayerController.gd
+++ Godot Project/scripts/PlayerController.gd
@@ -2,8 +2,8 @@ extends CharacterBody2D
 
 # Paddle properties
 var speed = 20000  # Adjust the speed of the paddle
-var maxX = 840   # Adjust the maximum X position of the paddle
-var minX = -100     # Adjust the minimum X position of the paddle
+var maxX = 480   # Adjust the maximum X position of the paddle
+var minX = -480     # Adjust the minimum X position of the paddle
 
 # Called every frame
 func _process(delta):
