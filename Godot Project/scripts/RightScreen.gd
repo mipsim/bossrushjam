cd6aa8fd25ff0daf9be9c592a0e892ca2a849581
mipsim <mip.k.sim@gmail.com>
2024-02-03 16:35:46 -0800

    0.0.6
    - Change face card values to 10
    - Add button to detect when a card is clicked on
    - Start working on detecting pairs among selected cards

--- Godot Project/scripts/RightScreen.gd
+++ Godot Project/scripts/RightScreen.gd
@@ -4,6 +4,10 @@ extends Node2D
 
 @onready var randomPicker = $RandomPicker
 
+var selected_cards: Array = [false,false,false,false,false]
+var card_values: Array = [0,0,0,0,0]
+var seen_numbers = {}
+
 func _ready():
 	randomize()
 
@@ -13,22 +17,37 @@ func _physics_process(delta):
 		var result1 = randomPicker.pick_random_item()
 		get_node("Card1/Frames").animation = str(result1)
 		get_node("Card1/Label").set_text((get_cardValue(result1)))
+		card_values[0] = get_cardValue(result1)
 		
 		var result2 = randomPicker.pick_random_item()
 		get_node("Card2/Frames").animation = str(result2)
 		get_node("Card2/Label").set_text((get_cardValue(result2)))
+		card_values[1] = get_cardValue(result2)
 		
 		var result3 = randomPicker.pick_random_item()
 		get_node("Card3/Frames").animation = str(result3)
 		get_node("Card3/Label").set_text((get_cardValue(result3)))
+		card_values[2] = get_cardValue(result3)
 		
 		var result4 = randomPicker.pick_random_item()
 		get_node("Card4/Frames").animation = str(result4)
 		get_node("Card4/Label").set_text((get_cardValue(result4)))
+		card_values[3] = get_cardValue(result4)
 		
 		var result5 = randomPicker.pick_random_item()
 		get_node("Card5/Frames").animation = str(result5)
 		get_node("Card5/Label").set_text((get_cardValue(result5)))
+		card_values[4] = get_cardValue(result5)
+		
+		# Check if there are duplicates in the hand of 5 cards
+		print("Cards on table: " + str(card_values))
+		for i in card_values:
+			if seen_numbers.has(i):
+				return true
+			else:
+				seen_numbers[i] = true
+		
+		print(seen_numbers)
 
 func get_cardValue(cardName):
 	var cardValue = 0
@@ -54,9 +73,32 @@ func get_cardValue(cardName):
 		"1":
 			cardValue = 10
 		"J":
-			cardValue = 11
+			cardValue = 10
 		"Q":
-			cardValue = 12
+			cardValue = 10
 		"K":
-			cardValue = 12
+			cardValue = 10
 	return str(cardValue)
+
+
+func _on_card_1_button_pressed():
+	if !selected_cards[0]:
+		selected_cards[0] = true
+		get_node("Card1/Frames").position.y = 528
+	else:
+		selected_cards[0] = false
+		get_node("Card1/Frames").position.y = 548
+	cardsScore()
+
+func _on_card_1_button_mouse_entered():
+	pass
+
+func _on_card_1_button_mouse_exited():
+	pass
+
+func cardsScore():
+	var selected_values: Array = []
+	
+	for i in card_values.size():
+		if selected_cards[i]:
+			selected_values.push_back(card_values[i])
